eab5024fb66e10470e97d302091a237d671cec2f
Nauman Ahmad <nauman-ahmad@outlook.com>
2014-04-19 19:25:53 +0500

    Uploaded All Files

---
+++ main.py
@@ -0,0 +1,37 @@
+#Import The Tweepy Module
+import tweepy
+
+class Twooly(object):
+
+    def __init__(self,API_KEY,API_SECRET,TOKEN,TOKEN_SECRET):
+        self.auth = tweepy.OAuthHandler(API_KEY,API_SECRET)
+        self.auth.set_access_token(TOKEN,TOKEN_SECRET)
+        self.twitter = tweepy.API(self.auth)
+        self.me = self.twitter.me()
+        self.name = self.me.name
+
+    def follow_followers(self):
+        self.followers = self.twitter.followers_ids(self.name)
+        self.following = self.twitter.friends_ids(self.name)
+
+        for each_follower in self.followers:
+            if each_follower not in self.following:
+                self.twitter.create_friendship(each_follower)
+                print "-" * 30
+                print "Following The User.."
+            else:
+                print "-" * 30
+                print "You Are Already Following Him!"
+
+    def auto_unfollow(self):
+        self.followers = self.twitter.followers_ids(self.name)
+        self.following = self.twitter.friends_ids(self.name)
+
+        for each_friend in self.following:
+            if each_friend not in self.followers:
+                self.twitter.destroy_friendship(each_friend)
+                print "-" * 30
+                print "Unfollowed The User.."
+            else:
+                print "-" * 30
+                print "He is following you.."
---
+++ setup.py
@@ -0,0 +1,10 @@
+import main
+
+API_KEY = "pKxKEHl6hyFwQBrh0GTZV8BBn"
+API_SECRET  ="sQ4xh1gZ8elA4Yq4sz8EtTlOZfT14FG3HH8RFMG8vHH9LI1XBU"
+TOKEN = "2447417772-Ul4fvWF7KZ3HM5LLKmM6yv81ZXJLsQfPUcpSU7A"
+TOKEN_SECRET = "I3unuCz3rlw4Jcf9f93r25SOkVXnvHf6ZggPXJVavrbCA"
+
+twooly = main.Twooly(API_KEY,API_SECRET,TOKEN,TOKEN_SECRET)
+a =  twooly.follow_followers()
+print a
